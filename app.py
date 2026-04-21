875aa26ab8a7f0fa6450b5b63710bc2ab8a71591
johndoe <johndoe@email.com>
2026-04-21 14:29:31 +1200

    testing database

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+import sqlite3
+
+db = sqlite3.connect("tree")
+cursor = db.cursor()
+sql = "SELECT * from treetable;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+db.close()
